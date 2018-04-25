c78e214005849c7e113c103feda8b26d2017f03c
UladKasach <nappa300@gmail.com>
2018-04-25 08:10:16 -0400

    get time, consume, and produce w/ tasks

--- src/main/main.c
+++ src/main/main.c
@@ -14,15 +14,46 @@
 
 //esp logging
 #include "esp_log.h"
-static const char *TAG = "app";
-static const char *TAG_RETREIVE = "retreive";
 
+// task handles
+xTaskHandle time_updater_task_handle;
+xTaskHandle producer_task_handle;
+xTaskHandle consumer_task_handle;
 
 // for udp server init socket
 int port_number = 3000;
 char* ip_addr = "192.168.43.48"; // ifconfig -a
 char* ssid = "Space Farms";
 char* pass = "Waterfall";
+int producer_delay_millisecond = 300;
+int consumer_delay_millisecond = 1000;
+int ids[2] = {0, 1};
+
+
+
+/*
+    tasks
+*/
+static void task_producer(void)
+{
+    for( ;; )
+    {
+        measure_parse_queue(ids);
+        milli_delay(producer_delay_millisecond);
+    }
+}
+static void task_consumer(void)
+{
+    //const int sleep_count = 10;
+    for( ;; )
+    {
+        output_data_from_queue();
+        milli_delay(consumer_delay_millisecond);
+    }
+}
+
+
+
 
 //Main application
 void app_main()
@@ -38,7 +69,7 @@ void app_main()
     initialize_udp_socket(port_number, ip_addr);
 
     // init queue
-    data_queue = xQueueCreate(QUEUE_SIZE, DATA_STRING_SIZE ); // create queue capable of holding 10 data strings at a time
+    data_queue = xQueueCreate(QUEUE_SIZE, DATA_STRING_SIZE); // create queue capable of holding 10 data strings at a time
     if(data_queue != 0){
         printf("%s\n", "data_queue initialized successfully");
     }
@@ -50,15 +81,51 @@ void app_main()
     register_sensor(0, 5, 2); // create lister for sensor 0: echo on 5, trig on 2
     register_sensor(1, 4, 0); // create lister for sensor 1: echo on 4, trig on 0
 
-    int index = 0;
-    while(1) {
-        index++;
-        int ids[2] = {0, 1};
-        measure_parse_queue(ids);
-        milli_delay(500);
-        if(index == 5){
-            output_data_from_queue();
-        }
+    /*
+        start tasks
+    */
+    // start time syncer
+    BaseType_t xReturned_updateTask;
+    xReturned_updateTask = xTaskCreate(task_update_internal_time_with_sntp,  // pointer to function
+                "time_update_task",        // Task name string for debug purposes
+                8000,            // Stack depth as word
+                NULL,           // function parameter (like a generic object)
+                1,              // Task Priority (Greater value has higher priority)
+                &time_updater_task_handle);  // Task handle
+    if( xReturned_updateTask == pdPASS )
+    {
+        printf("time updater task started successfully\n");
     }
+
+    // wait untill the time is updated
+    wait_until_time_updated();
+
+    // start producer task
+    BaseType_t xReturned_producer_task;
+    xReturned_producer_task = xTaskCreate(task_producer,  // pointer to function
+                "pulse_task",        // Task name string for debug purposes
+                4000,            // Stack depth as word
+                NULL,           // function parameter (like a generic object)
+                2,              // Task Priority (Greater value has higher priority)
+                &producer_task_handle);  // Task handle
+    if( xReturned_producer_task == pdPASS )
+    {
+        printf("producer task started successfully\n");
+    }
+
+
+    // start consumer task
+    BaseType_t xReturned_consumer_task;
+    xReturned_consumer_task = xTaskCreate(task_consumer,  // pointer to function
+                "time_output_task",        // Task name string for debug purposes
+                8000,            // Stack depth as word
+                NULL,           // function parameter (like a generic object)
+                2,              // Task Priority (Greater value has higher priority)
+                &consumer_task_handle);  // Task handle
+    if( xReturned_consumer_task == pdPASS )
+    {
+        printf("consumer task started successfully\n");
+    }
+
 
 }
